31e85b62069977eef60f3676dd9d076ba24f4beb
v_ktlema <v_ktlema@tencent.com>
2022-03-30 13:47:20 +0800

    提交

---
+++ src/main/java/com/mzl/incomeexpensemanagesystem1/IncomeExpenseManageSystem1Application.java
@@ -0,0 +1,13 @@
+package com.mzl.incomeexpensemanagesystem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeExpenseManageSystem1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncomeExpenseManageSystem1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mzl/incomeexpensemanagesystem1/IncomeExpenseManageSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzl.incomeexpensemanagesystem1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeExpenseManageSystem1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
